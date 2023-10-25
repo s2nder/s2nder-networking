595ee1bb3ba587d96a4dcab4f61da313f92af0ac
s2nder <83005169+s2nder@users.noreply.github.com>
2023-10-24 20:14:25 -0400

    Add workspace tag and make variables required.

--- variables.tf
+++ variables.tf
@@ -28,14 +28,14 @@ variable "billing_code" {
 variable "cidr_block" {
   type        = string
   description = "(Optional) The CIDR block for the VPC. Default:10.42.0.0/16"
-  default     = "10.42.0.0/16"
+  #default     = "10.42.0.0/16"
 }
 
 variable "public_subnets" {
   type        = map(string)
   description = "(Optional) Map of public subnets to create with CIDR blocks. Key will be used as subnet name with prefix. Default: {subnet-1 ="
-  default = {
-    public-1 = "10.42.10.0/24"
-    public-2 = "10.42.11.0/24"
-  }
+  #default = {
+  #  public-1 = "10.42.10.0/24"
+  #  public-2 = "10.42.11.0/24"
+  #}
 }
